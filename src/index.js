a360866df2c19134a2e70a8a1cc181f94952b003
kylefarmer85 <kylefarmer85@gmail.com>
2020-10-29 11:00:13 -0500

    first

--- src/index.js
+++ src/index.js
@@ -1,8 +1,26 @@
 let addToy = false;
+const form = document.querySelector('.add-toy-form')
+const addBtn = document.querySelector("#new-toy-btn");
+const toyFormContainer = document.querySelector(".container");
 
-document.addEventListener("DOMContentLoaded", () => {
-  const addBtn = document.querySelector("#new-toy-btn");
-  const toyFormContainer = document.querySelector(".container");
+function main() {
+  fetchToys()
+  renderToys()
+
+}
+
+function fetchToys() {
+  fetch('http://localhost:3000/toys')
+    .then(resp => resp.json())
+    .then(json => renderToys(json));
+}
+
+function renderToys(toys) {
+
+}
+
+
+
   addBtn.addEventListener("click", () => {
     // hide & seek with the form
     addToy = !addToy;
@@ -12,4 +30,27 @@ document.addEventListener("DOMContentLoaded", () => {
       toyFormContainer.style.display = "none";
     }
   });
-});
+
+
+form.addEventListener('submit', function(e) {
+  e.preventDefault()
+
+  fetch('http://localhost:3000/toys', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
+    },
+    body: JSON.stringify({
+      name: e.target["name"].value,
+      image: e.target["image"].value
+    })
+  })
+  e.target.reset()
+
+})
+
+
+
+
+main()
